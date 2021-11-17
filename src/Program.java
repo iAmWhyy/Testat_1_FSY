8ab2467a926d26556b9fee0469bac5e31d717e37
Yannick <iamwhy@gmx.de>
2021-11-17 22:55:00 +0100

    usage of swap method

--- src/Program.java
+++ src/Program.java
@@ -61,10 +61,7 @@ class ArrayInputCreator {
         for (int i = ar.length - 1; i > 0; i--)
         {
             int index = rnd.nextInt(i + 1);
-            // Simple swap
-            int a = ar[index];
-            ar[index] = ar[i];
-            ar[i] = a;
+            swap(ar, index, i);
         }
     }
 
